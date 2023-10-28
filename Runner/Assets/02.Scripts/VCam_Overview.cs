f5bb4e61675df0a0d6bdf01f9d1b0c87c1990222
eunsong415 <143861768+shoungli415@users.noreply.github.com>
2023-10-28 17:30:44 +0900

    Runner_cinematic play

---
+++ Runner/Assets/02.Scripts/VCam_Overview.cs
@@ -0,0 +1,19 @@
+using Cinemachine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VCam_Overview : MonoBehaviour
+{
+    private CinemachineVirtualCameraBase _vCam;
+
+    private void Awake()
+    {
+        _vCam = GetComponent<CinemachineVirtualCameraBase>();
+    }
+
+    private void OnEnable()
+    {
+        _vCam.Follow = _vCam.LookAt = PlayManager.instance.lead.transform;
+    }
+}
